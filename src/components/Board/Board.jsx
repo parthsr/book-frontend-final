df03a94135e19e3f65880485f377a58789aef62d
parth <srivastavaparth2016@gmail.com>
2018-02-21 13:48:15 +0530

    final changes

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -6,14 +6,17 @@ import Container from '../Container/Container';
 import './Board.css';
 
 class Board extends React.Component {
-  componentDidMount =() => {
-    axios.get('/allbooks').then((results) => {
-      this.props.getBooks(results.data);
-    });
-  }
+  // componentDidMount =() => {
+  //   axios.get('/allbooks').then((results) => {
+  //     this.props.getBooks(results.data);
+  //   });
+  // }
   render() {
-    if (this.props.books === {}) {
-      return (<p>hi</p>);
+    if (Object.keys(this.props.books).length === 0 && this.props.books.constructor === Object) {
+      axios.get('/allbooks').then((results) => {
+        this.props.getBooks(results.data);
+      });
+      return (<h1>hi</h1>);
     }
     return (
       <div className="Board-Column">
