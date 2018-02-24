febfb093df9e1c7a02c0764c69bd2cfafad041d8
parth <srivastavaparth2016@gmail.com>
2018-02-25 03:04:07 +0530

    Header of the project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <div className="Header-title-wrapper">
+    <div className="Header-title">
+      <p>The </p>
+      <h1>Book </h1>
+      <p>Shelf</p>
+    </div>
+  </div>
+);
+
+export default Header;
