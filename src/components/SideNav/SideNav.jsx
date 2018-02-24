67b09595ff9e7f53ce337fc13f727eab01bd07ad
parth <srivastavaparth2016@gmail.com>
2018-02-25 03:04:37 +0530

    Side nav component

---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SideNav.css';
+
+const SideNav = () => (
+  <div className="Board-div" >
+    <p className="Board-bs">Bs</p>
+    <div className="Board-button">
+      <p><i className="material-icons">refresh</i></p>
+      <i className="material-icons">settings</i>
+    </div>
+  </div>
+);
+
+export default SideNav;
